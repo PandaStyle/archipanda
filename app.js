977ec897beb1246f8f99b2090bf2cc5566dd326f
Zsolt Nemeth <Zsolt.Nemeth@logmein.com>
2015-11-18 18:31:37 -0500

    remove localhost from hapi server config

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ var InstagramService = require('./services/instagram/instagram.js');
 // Create a server with a host and port
 var server = new Hapi.Server();
 server.connection({
-    host: 'localhost',
     port: 8081,
     routes: {
         cors: {
